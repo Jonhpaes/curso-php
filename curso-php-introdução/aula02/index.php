c2e516e50ce475eacc3dda3a336398068198b760
Jonh Alex <jonhpaz08@gmail.com>
2022-04-22 03:17:28 -0300

    Update index.php

--- curso-php-introdução/aula02/index.php
+++ curso-php-introdução/aula02/index.php
@@ -1,6 +1,38 @@
 <?php
 
-$categorias = {};
-$categorias() = 'infantil';
-$categorias() = 'adolescente';
-$categorias() = 'adulto';
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+//print_r($categorias); //explicação: exibe o formato/sqnc do código
+
+
+$nome = 'Eduardo';
+$idade = 17;
+
+//var_dump($nome); //explixação: informações sobre oque temos
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+            echo "O nadador ",$nome. " compete na categoria infantil";
+    }
+}
+else if($idade >= 13 && $idade <= 17)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+            echo "O nadador ",$nome. " compete na categoria adolescente";
+    }
+}
+else {
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+            echo "O nadador ",$nome. " compete na categoria adulto";
+    }
+}
